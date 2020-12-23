7662d4fac03d1516acf8edd3044a9bbb2f80f267
Wiktor Religo <wiciorny@gmail.com>
2020-12-23 22:42:34 +0100

    set up project for PackMan game

--- AjaxChapter/ajaxMain.js
+++ AjaxChapter/ajaxMain.js
@@ -1,7 +1,19 @@
-// var el = document.getElementsByClassName('btn-info')[0];
-// el.addEventListener('click', loadText);
+document.addEventListener('DOMContentLoaded', function () {
+      var el = document.getElementsByClassName('btn-info')[0];
+      el.addEventListener('click', loadText);
+}
+)
 
-// function loadText() {
-
-
-// }
+function loadText() {
+      xhr = new XMLHttpRequest();
+      xhr.open('GET', 'user.json', true);
+      xhr.onload = function () {
+            if (xhr.status == 200) {
+                  var users = this.responseText;
+                  var parsed = JSON.parse(this.responseText);
+                  console.log(users);
+                  console.log(parsed)
+            }
+      }
+      xhr.send();
+}
