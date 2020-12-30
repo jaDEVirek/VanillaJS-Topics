39f58969cd392468e867cdf4ac973cadbcf83262
Wiktor Religo <wiciorny@gmail.com>
2020-12-30 21:06:55 +0100

    setting up post example for php ajax, starting wokring with mysql

--- AjaxChapter/phpAjax.js
+++ AjaxChapter/phpAjax.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
       var el = document.getElementsByClassName('btn-primary')[0];
       var el2 = document.getElementById('st-form');
+      var el3 = document.getElementById('sd-form');
       el.addEventListener('click', loadPHPButton);
       el2.addEventListener('submit', validForms);
+      el3.addEventListener('submit', postForms);
 })
 
 function loadPHPButton() {
@@ -19,14 +21,11 @@ function loadPHPButton() {
 function validForms(e) {
       e.preventDefault();
       var name = document.querySelector('#st-form input[name="name"]').value;
-      console.log('check name ' + name);
-
       xhr = new XMLHttpRequest();
       xhr.open('GET', 'process.php?name=' + name, true);
       xhr.onload = function () {
             let outPut = '';
             if (xhr.status == 200) {
-                  console.log(this.responseText);
                   outPut += "<span>" +
                         this.responseText +
                         "</span>";
@@ -34,6 +33,19 @@ function validForms(e) {
             document.querySelector('.col.d-block').innerHTML = outPut;
       }
       xhr.send();
+}
+
+function postForms(e) {
+      e.preventDefault();
+      var name = document.querySelector('#sd-form input[name="name"]').value;
+      var rqParam = "name=" + name;
+      xhr = new XMLHttpRequest();
+      xhr.open('POST', 'process.php', true);
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      xhr.onload = function () {
+            console.log(this.responseText);
+      }
+      xhr.send(rqParam);
 }
 
 
--- AjaxChapter/process.php
+++ AjaxChapter/process.php
@@ -1,7 +1,24 @@
 <?php
 echo 'Processing...';
 
-//Check getting param: 
+
+//db connection mySQL : DB: AjaxTests
+$conn = mysqli_connect('localhost','root','123456','AjaxTests');
+
+//Check getting param FOR GET: 
 if(isset($_GET['name'])){
       echo 'GET: Your name is:  '. $_GET['name'];
 }
+
+//Check getting param FOR POST: 
+if(isset($_POST['name'])){
+      $name= mysqli_real_escape_string($conn,$_POST['name']);
+      echo 'POST: Your name is:  '. $_POST['name'];
+      $query = "INSERT INTO users(name) VALUES ('$name')";
+      if(mysqli_query($conn,$query)){
+            echo 'User Added,,,,';  
+      }else{
+            echo '';
+      }
+}
+?>
