77e3dbbcede4ff165be60b00225eba351da6f5d4
Wiktor Religo <wiciorny@gmail.com>
2021-01-15 23:38:30 +0100

    adding example of binding, this, and lexical scope in arrow function or normal

--- FunctionalChapter/MoreArrowFunctions/arrows.js
+++ FunctionalChapter/MoreArrowFunctions/arrows.js
@@ -20,7 +20,6 @@ me.thisInRegular();
 
 function person() {
       //  let that = this;
-
       var age = 10;
       function logs() {
             console.log(this);
@@ -30,3 +29,40 @@ function person() {
       console.log(this);
 }
 person();
+// Lambda functions
+// this and arguments are lexical 
+function Playlist(name) {
+      this.name = name;
+}
+
+Playlist.prototype.play = function (songs) {
+      return songs.map(function (song) {
+            console.log(this);
+
+            return `Playlist  ${this.name} - ${song.title}`;
+      });
+}
+Playlist.prototype.oldThatPlay = function (songs) {
+      var that = this;
+      return songs.map(function (song) {
+            console.log(that);
+            return `Playlist  ${that.name} - ${song.title}`;
+      });
+}
+Playlist.prototype.arrowThisPlay = function (songs) {
+      return songs.map((song) => {
+            console.log(this);
+            return `Playlist  ${this.name} - ${song.title}`;
+      });
+}
+Playlist.prototype.bindPlay = function (songs) {
+      return songs.map(function (song) {
+            console.log(this);
+            return `Playlist  ${this.name} - ${song.title}`;
+      }.bind(this));
+}
+let myPlaylist = new Playlist("Europe");
+console.log(myPlaylist.play([{ title: 'Carrie' }, { title: 'Final countdown' }]));
+console.log(myPlaylist.oldThatPlay([{ title: 'Carrie' }, { title: 'Final countdown' }]));
+console.log(myPlaylist.arrowThisPlay([{ title: 'Carrie' }, { title: 'Final countdown' }]));
+console.log(myPlaylist.bindPlay([{ title: 'Carrie' }, { title: 'Final countdown' }])); 
